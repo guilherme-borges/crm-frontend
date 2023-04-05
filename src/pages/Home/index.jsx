528c5739fbe3958b322b0d9a64f59e8459d73387
Guilherme Borges <ti@dse.com.br>
2023-04-05 17:32:05 -0300

    Listagem de oportunidades

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,45 +1,68 @@
 import Menu from '../../components/Menu'
+import axios from 'axios'
+import { useState, useEffect } from 'react'
 
 function Home() {
+
+  const [oportunidades, setOportunidades] = useState([])
+
+  useEffect(() => {
+    buscarOportunidades()
+  }, [])
+
+  const instance = axios.create({
+    baseURL: 'http://localhost:8080/api/v1'
+  })
+
+  async function buscarOportunidades() {
+    try {
+      const response = await instance.get('/oportunidades')
+      setOportunidades(response.data)
+      console.log(oportunidades);
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
   return (
     <>
       <Menu />
 
-      <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h1 class="modal-title fs-5" id="exampleModalLabel">Cadastro de Oportunidade</h1>
-              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      <div className="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h1 className="modal-title fs-5" id="exampleModalLabel">Cadastro de Oportunidade</h1>
+              <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
-            <div class="modal-body">
-              <div class="mb-3">
-                <label for="cbxClientes" class="form-label">Clientes</label>
-                <select class="form-select" aria-label="Default select example" id="cbxClientes">
+            <div className="modal-body">
+              <div className="mb-3">
+                <label htmlFor="cbxClientes" className="form-label">Clientes</label>
+                <select className="form-select" aria-label="Default select example" id="cbxClientes">
                   <option selected>Selecione o cliente</option>
                   <option value="1">Cliente 1</option>
                   <option value="2">Cliente 2</option>
                   <option value="3">Cliente 3</option>
                 </select>
               </div>
-              <div class="mb-3">
-                <label for="cbxProjetos" class="form-label">Projetos</label>
-                <select class="form-select" aria-label="Default select example" id="cbxProjetos">
+              <div className="mb-3">
+                <label htmlFor="cbxProjetos" className="form-label">Projetos</label>
+                <select className="form-select" aria-label="Default select example" id="cbxProjetos">
                   <option selected>Selecione o projeto</option>
                   <option value="1">Projeto 1</option>
                   <option value="2">Projeto 2</option>
                   <option value="3">Projeto 3</option>
                 </select>
               </div>
-              <label for="txtValor" class="form-label">Valor</label>
-              <div class="input-group mb-3">
-                <span class="input-group-text">R$</span>
-                <input type="number" class="form-control" id="txtValor" />
+              <label htmlFor="txtValor" className="form-label">Valor</label>
+              <div className="input-group mb-3">
+                <span className="input-group-text">R$</span>
+                <input type="number" className="form-control" id="txtValor" />
               </div>
             </div>
-            <div class="modal-footer">
-              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
-              <button type="button" class="btn btn-primary">Salvar</button>
+            <div className="modal-footer">
+              <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
+              <button type="button" className="btn btn-primary">Salvar</button>
             </div>
           </div>
         </div>
@@ -47,12 +70,12 @@ function Home() {
 
       <main className="container mt-5">
         <div className="d-flex justify-content-end">
-          <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-            <i class="bi bi-plus-lg"></i>
+          <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+            <i className="bi bi-plus-lg"></i>
             Novo
           </button>
         </div>
-        <table class="table">
+        <table className="table">
           <thead>
             <tr>
               <th scope="col">Editar</th>
@@ -63,27 +86,24 @@ function Home() {
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td><button type="button" class="btn btn-success"><i class="bi bi-pencil-fill"></i></button></td>
-              <td><button type="button" class="btn btn-danger"><i class="bi bi-trash-fill"></i></button></td>
-              <td>Projeto 1</td>
-              <td>Cliente 1</td>
-              <td>R$ 2000,00</td>
-            </tr>
-            <tr>
-              <td><button type="button" class="btn btn-success"><i class="bi bi-pencil-fill"></i></button></td>
-              <td><button type="button" class="btn btn-danger"><i class="bi bi-trash-fill"></i></button></td>
-              <td>Projeto 2</td>
-              <td>Cliente 2</td>
-              <td>R$ 4000,00</td>
-            </tr>
-            <tr>
-              <td><button type="button" class="btn btn-success"><i class="bi bi-pencil-fill"></i></button></td>
-              <td><button type="button" class="btn btn-danger"><i class="bi bi-trash-fill"></i></button></td>
-              <td>Projeto 3</td>
-              <td>Cliente 3</td>
-              <td>R$ 3000,00</td>
-            </tr>
+
+            {
+              oportunidades.map((oportunidade) => {
+                <tr>
+                  <td>
+                    <button
+                      type="button"
+                      className="btn btn-success"
+                    >
+                      <i className="bi bi-pencil-fill"></i></button></td>
+                  <td><button type="button" className="btn btn-danger"><i className="bi bi-trash-fill"></i></button></td>
+                  <td>{oportunidade.projeto.nome}</td>
+                  <td>{oportunidade.cliente.nome}</td>
+                  <td>R$ {oportunidade.valor}</td>
+                </tr>
+              })
+            }
+
           </tbody>
         </table>
       </main>
